07c7746519f7814f8629abd364f550da850cafe9
Damse96 <eaaankd@students.eaaa.dk>
2023-12-10 12:47:07 +0100

    Test

---
+++ Test/ControllerTest.java
@@ -0,0 +1,22 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void opretMedarbejder() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void opretDestillering() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void opretPåfyldning() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void opretMængde() {
+    }
+}
---
+++ src/application/model/Whisky.java
@@ -0,0 +1,15 @@
+package application.model;
+
+public class Whisky {
+    private String navn;
+
+    private int nr;
+
+    private double procent;
+
+    Påfyldning påfyldning;
+
+    private double tilsætningAfVand;
+
+
+}
